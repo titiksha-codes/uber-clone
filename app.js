92c21f575a06a5150afebe11c3d48a99ea939022
Titiksha Basu <131252499+titiksha-codes@users.noreply.github.com>
2025-02-17 15:57:54 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const connectToDb = require('./db/db');
+
+connectToDb();
+
+app.use(cors());
+
+
+app.get('/', (req,res) => {
+    res.send('hello world');
+});
+
+module.exports = app;
+
